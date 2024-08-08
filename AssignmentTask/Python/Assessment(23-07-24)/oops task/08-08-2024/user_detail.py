1cf853929a9c4ef54ab12ca3b28179007567adfc
mmprasanth17 <prasanthmani1711@gmail.com>
2024-08-08 12:17:28 +0530

    08-08-2024 changed

--- AssignmentTask/Python/Assessment(23-07-24)/oops task/08-08-2024/user_detail.py
+++ AssignmentTask/Python/Assessment(23-07-24)/oops task/08-08-2024/user_detail.py
@@ -15,15 +15,17 @@ class User:
     def describe_user(self):
         print(f'user profile \n Name : {self.first_name} {self.last_name}\n Email : {self.email}\n Phone No:{self.phone_no}')
     
-    @staticmethod
     def greet_user():
         print(f'Hey guys Above details are my user profile')
-        
-profile1=User("Prasanth","Mani","prasanth@gmail.com","942316345")
-profile2=User("Mani","Kanniyappan","mani@gmail.com","842316345")
-profile3=User("Roja","Mani","roja@gmail.com","742316345")
+def main():      
+    profile1=User("Prasanth","Mani","prasanth@gmail.com","942316345")
+    profile2=User("Mani","Kanniyappan","mani@gmail.com","842316345")
+    profile3=User("Roja","Mani","roja@gmail.com","742316345")
 
-User.greet_user()
-profile1.describe_user()
-profile2.describe_user()
-profile3.describe_user()
+    User.greet_user()
+    profile1.describe_user()
+    profile2.describe_user()
+    profile3.describe_user()
+    
+if __name__=="__main__":
+    main()
